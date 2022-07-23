ade70f61b95d8bd614f2a4cef79c544107d3ec9d
ankr19 <ankm335@gmail.com>
2022-07-23 08:04:49 +0530

    initial commit
    adding the user schema

--- Restrowith/backend/app.js
+++ Restrowith/backend/app.js
@@ -1,11 +1,15 @@
-// const connectToMongo = require('./db');
+const connectToMongo = require('./db/db');
 const express = require('express')
-// connectToMongo();
+require('dotenv').config({ path: './app.env' })
+
+connectToMongo();
 const app = express()
 const port = 5000
 app.get('/', (req, res) => {
     res.send('Hello harry')
 })
+console.log("secret key",process.env.secret_key);
+console.log("secret key",process.env.secret_token);
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
